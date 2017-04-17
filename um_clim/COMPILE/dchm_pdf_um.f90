364f6bd8825d80eecc997e43dcf59f231a03ca37
kyha0830 <youngha.kim0830@gmail.com>
2017-04-18 03:46:08 +0900

    pdf revised

--- um_clim/COMPILE/dchm_pdf_um.f90
+++ um_clim/COMPILE/dchm_pdf_um.f90
@@ -5,28 +5,32 @@ PROGRAM DCHM_PDF
 
   implicit none
 
-  integer, parameter ::  nv = 4
+  integer, parameter ::  nv = 15, nstat = 3, nzu = 33
+
+  real ::  pdf_rng_ln10(3)
 
   namelist /ANALCASE/ EXPNAME, YYYY, MM, HH, REFDATE, OPT_30D
-!  namelist /PARAM/ 
+  namelist /PARAM/ PDF_RNG_LN10, LAT_RNG
   namelist /FILEIO/ DAY1, NDAY_I, MISSV, FID, FILE_I_HEAD, FILE_I_FORM,  &
-                    FILE_I_XXXX, VAR_I_NAME, FILE_O
+                    FILE_I_XXXX, VAR_I_NAME, FILE_ALT, VAR_ALT, FILE_O
 
-  integer ::  imon, ihour, i_time, npop, npdfx
-  integer ::  j,k
+  integer ::  imon, ihour, i_time, npdfx, iy2(2), ny2, ncol, ncolm
+  real    ::  dpdfx
+  integer ::  i,j,k,l,ib
   character(len=32), dimension(nv) ::  ovarname
 
-  real, dimension(:,:,:), allocatable ::  varx
-  real, dimension(:,:,:), allocatable ::  var3d, var3d2, var3d3
+  real   , dimension(:,:,:), allocatable ::  vari, var_m, var_u, var_v
+  real   , dimension(:,:)  , allocatable ::  vars
+  real   , dimension(:)    , allocatable ::  pdfx, varx
+  integer, dimension(:)    , allocatable ::  pdfi, npop
 
-  type(vset), dimension(nv) ::  set
+  type(vset), dimension(nv+1) ::  set
 
 ! READ NAMELISTS
 
   call getarg(1,f_namelist)
   open(10, file=trim(f_namelist), status='old')
-!  read(10, ANALCASE)  ;  read(10, PARAM)  ;  read(10, FILEIO)
-  read(10, ANALCASE)  ;  read(10, FILEIO)
+  read(10, ANALCASE)  ;  read(10, PARAM)  ;  read(10, FILEIO)
   close(10)
 
 ! GET AXES AND INITIALIZE ARRAYS
@@ -36,6 +40,9 @@ PROGRAM DCHM_PDF
 
   call initialize
 
+  npop(:) = 0
+  var_m(:,:,:) = 0.
+ 
   i_time = 0
 
   L_MON:  DO imon=1, nmon
@@ -53,15 +60,31 @@ PROGRAM DCHM_PDF
 
   day_from_ref = get_dayfromref(year,mon,date,hour)
 
-  iv_i = 1  ! for get_ifilename
-  file_i(iv_i) = get_ifilename()
-  inquire(file=trim(file_i(iv_i)), exist=ex1)
-  if ( .not. ex1 ) then
-    print*, '    ',trim(file_i(iv_i)),' not found.'  ;  STOP
+  ! problem found in the data for 00 UTC 1 each month
+  if ( date == 1 .and. hour == 0 ) then
+    hour = hour + 24/nhour
+    CYCLE
   end if
- 
+
   ! get variable
   call get_1var
+
+  if (ncol == 0) then
+    hour = hour + 24/nhour
+    CYCLE
+  end if
+
+  pdfi(:ncol) = (int( (varx(:ncol) - pdf_rng_ln10(1))/dpdfx + 1.5 )-1)+2
+  do l=1, ncol
+    pdfi(l) = min(npdfx,max(1,pdfi(l)))
+  enddo
+
+  do l=1, ncol
+    ib = pdfi(l)
+    npop(ib) = npop(ib) + 1
+    var_m(ib,:,1) = var_m(ib,:,1) + vars(l,:)
+    var_m(ib,:,2) = var_m(ib,:,2) + vars(l,:)*vars(l,:)
+  enddo
 
   hour = hour + 24/nhour
 
@@ -78,30 +101,48 @@ PROGRAM DCHM_PDF
   !---------------------------------------------------------------------
   ENDDO  L_MON
 
-  nt = i_time - 1
+  nt = i_time
 
-  deallocate( varx )
+  deallocate( varx, vars, vari, pdfi )
+
+  do ib=1, npdfx
+    if (npop(ib) /= 0)  var_m(ib,:,:2) = var_m(ib,:,:2)/float(npop(ib))
+  enddo
+  var_m(:,:,3) = var_m(:,:,2) - var_m(:,:,1)*var_m(:,:,1)
+
+  write(6,*) 'total columns  : ', int(sum(npop(:))), '/', nx*ny2*nt
+  write(6,*) 'inside the PDF : ', int(sum(npop(2:npdfx-1)))
+  write(6,*) 'outside        : ', int(npop(1)), int(npop(npdfx))
 
 
-  nd1a = NPOP
-  nd2a = NPDFX
+  nd1a = NPDFX
+  nd2a = NSTAT
   nd3a = 1
   nd4a = 1
+
+  pdfx(1    ) = pdfx(2      ) - dpdfx*10.
+  pdfx(npdfx) = pdfx(npdfx-1) + dpdfx*10.
+  pdfx(:) = 10.**pdfx(:)
 
   do iv=1, nv
     call setdim
     allocate( set(iv)%var_out(nd1a,nd2a,nd3a,nd4a) )
     set(iv)%var_out(:,:,:,:) = 1.e32
 
-    set(iv)%var_out(:,:,1,1) = var3d(:,:,iv)
+    set(iv)%var_out(:,:,1,1) = var_m(:,iv,:)
   enddo
+
+  iv = nv+1
+  call setdim_npop
+  allocate( set(iv)%var_out(nd1a,1,1,1) )
+  set(iv)%var_out(:,1,1,1) = float(npop(:))
 
 
 ! DUMP
 
   write(6,*)  ;  write(6,*) trim(file_o)  ;  write(6,*)
 
-  call outnc(trim(file_o),nv,set,'mean and variances of p at model levels')
+  call outnc(trim(file_o),nv+1,set,'PDF for '//trim(var_i_name(1)))
 
 ! END
 
@@ -117,7 +158,9 @@ PROGRAM DCHM_PDF
 
   nmon = mm(2)  ;  nhour = hh(2)
 
-  ndate = 30  ;  date = 1  ;  hour = hh(1)  ! for get_ifilename
+!  problem found in the data for 00 UTC 1 each month
+!  ndate = 30  ;  date = 1  ;  hour = hh(1)  ! for get_ifilename
+  ndate = 30  ;  date = 2  ;  hour = hh(1)  ! for get_ifilename
   if (opt_30d == 0)  ndate = get_ndate()
   day_from_ref = get_dayfromref(year,mon,date,hour)
 
@@ -129,44 +172,122 @@ PROGRAM DCHM_PDF
   end if
   call getdim(file_i(iv_i),var_i_name(iv_i))
 
-  allocate( var3d(npop,npdfx,nv), var3d2(npop,npdfx,nv), var3d3(npop,npdfx,nv) )
-  var3d(:,:,:) = 0.  ;  var3d2(:,:,:) = 0.  ;  var3d3(:,:,:) = 0.
-  allocate( varx(nx,ny,1) )
+  call get_iouter(lat,lat_rng, iy2)
 
-  ovarname = (/'p_m   ','dp2_m','p2_m','sd_loc'/)
+  ny2 = iy2(2) - iy2(1) + 1
+
+  npdfx = int(pdf_rng_ln10(3))+2
+  dpdfx = (pdf_rng_ln10(2) - pdf_rng_ln10(1))/(pdf_rng_ln10(3)-1)
+  allocate( pdfx(npdfx) )
+  pdfx(1) = -999.
+  do ib=2, npdfx-1
+    pdfx(ib) = pdf_rng_ln10(1) + (ib-2)*dpdfx
+  enddo
+  pdfx(npdfx) = 999.
+
+  allocate( npop(npdfx), var_m(npdfx,nv,nstat) )
+  npop(:) = 0  ;  var_m(:,:,:) = 0.
+
+  x1_i = 1   ;  y1_i = iy2(1)   ;  z1_i = 1       ! for getalt
+  nx_i = nx  ;  ny_i = ny2      ;  nz_i = nzu-1
+  call getalt
+  do k=1, nzu-1
+    z_th(:,:,k) = z_th(:,:,k) - z_th(:,:,0)
+  enddo
+  z_th(:,:,0) = 0.
+
+  ncolm = nx*ny2
+
+  allocate( vari(nx,ny2,nv), varx(ncolm), vars(ncolm,nv), pdfi(ncolm) )
+  allocate( var_u(nx,ny2,nzu), var_v(nx,ny2,nzu) )
+
+  ovarname = (/'dchmax','zcba  ','zcta  ','rho_ct','n_q   ','n_ct  ',    &
+               't_ct  ','cq_x  ','cq_y  ','u_ct  ','v_ct  ','u_sfc ',    &
+               'v_sfc ','u_cb  ','v_cb  '/)
 
   END subroutine initialize
 
   SUBROUTINE get_1var
 
-  nx_i = nx  ;  ny_i = ny  ;  nz_i = nz   ! for get_ivar3d
-
+  ex0 = .TRUE.
+  do iv_i=1, 4  ! for get_ifilename
+    file_i(iv_i) = get_ifilename()
+    inquire(file=trim(file_i(iv_i)), exist=ex1)
+    if ( .not. ex1 )  print*, '    ',trim(file_i(iv_i)),' not found.'
+    ex0 = ( ex0 .and. ex1 )
+  enddo
+  if (.not. ex0)  STOP
+ 
   ! read var.
   print*, trim(file_i(1))
-  iv_i = 1  ;  varx(:,:,:) = get_ivar3d()
+
+  iv_i = 1  ;  vari(:,:,1:1) = get_ivara3d(1,nx,iy2(1),ny2,1,1)
+
+  iv_i = 2  ;  vari(:,:,2:11) = get_ivara3d(1,nx,iy2(1),ny2,1,10)
+
+  iv_i = 3  ;  var_u(:,:,:) = get_ivara3d(1,nx,iy2(1),ny2,1,nzu)
+  iv_i = 4  ;  var_v(:,:,:) = get_ivara3d(1,nx,iy2(1),ny2,1,nzu)
+
   print*, 'time index :', it_i(1)
+
+  vari(:,:,1) = vari(:,:,1)*3600.  ! [K/hour]
+
+  varx(:) = 0.  ;  vars(:,:) = 0.
+
+  l = 0
+  do j=1, ny2
+  do i=1, nx
+    if (vari(i,j,1) == missv)  CYCLE
+    l = l + 1
+    varx(l) = vari(i,j,1)
+    vars(l,:11) = vari(i,j,:)
+    vars(l,12) = var_u(i,j,1)  ! u_sfc
+    vars(l,13) = var_v(i,j,1)  ! v_sfc
+    k = minloc(abs(vari(i,j,2) - z_th(i,j,:)),1)-1  ! zcba: vari(:,:,2)
+    vars(l,14) = 0.5*(var_u(i,j,k) + var_u(i,j,k+1))  ! u_cb
+    vars(l,15) = 0.5*(var_v(i,j,k) + var_v(i,j,k+1))  ! v_cb
+  enddo
+  enddo
+  ncol = l
+  if (ncol /= 0)  varx(:ncol) = log10(varx(:ncol))
 
   END subroutine get_1var
 
   SUBROUTINE setdim
 
   set(iv)%vname = trim(ovarname(iv))
-  set(iv)%axis = (/'lat  ','z ',' ',' '/) 
+  set(iv)%axis = (/'x_pdf','stat',' ',' '/) 
   set(iv)%nd(:) = (/nd1a,nd2a,nd3a,nd4a/)
   allocate( set(iv)%axis1(set(iv)%nd(1)) )
   allocate( set(iv)%axis2(set(iv)%nd(2)) )
   allocate( set(iv)%axis3(set(iv)%nd(3)) )
   allocate( set(iv)%axis4(set(iv)%nd(4)) )
-  set(iv)%axis1 = lat
-  set(iv)%axis2 = ht
+  set(iv)%axis1 = pdfx(:)
+  set(iv)%axis2 = (/1.,2.,3/)
   set(iv)%axis3 = -999.
   set(iv)%axis4 = -999.
     
   END subroutine setdim
 
+  SUBROUTINE setdim_npop
+
+  set(iv)%vname = 'N_pop'
+  set(iv)%axis = (/'x_pdf',' ',' ',' '/) 
+  set(iv)%nd(:) = (/npdfx,1,1,1/)
+  allocate( set(iv)%axis1(npdfx) )
+  allocate( set(iv)%axis2(1) )
+  allocate( set(iv)%axis3(1) )
+  allocate( set(iv)%axis4(1) )
+  set(iv)%axis1 = pdfx(:)
+  set(iv)%axis2 = -999.
+  set(iv)%axis3 = -999.
+  set(iv)%axis4 = -999.
+    
+  END subroutine setdim_npop
+
   SUBROUTINE finalize
 
-  deallocate( var3d, var3d2, var3d3 )
+  deallocate( var_m, npop, pdfx )
   deallocate( lon, lat, ht, ht_th )
   do iv=1, nv
     deallocate( set(iv)%axis1, set(iv)%axis2, set(iv)%axis3,             &
--- um_clim/qboi/um_dchm_pdf.sh
+++ um_clim/qboi/um_dchm_pdf.sh
@@ -13,23 +13,27 @@ M1=$EXTCTL1
 M2=$EXTCTL2
 #== Parameter 1 - CASE ===========================
 P101=( s EXPNAME  $expcode )
-P104=( n HH  00  8 ) # should be 0 # starting time [UTC] and frequency [/day]
+P104=( n HH  03  8 ) # should be 0 # starting time [UTC] and frequency [/day]
 P105=( n REFDATE  1941  12  1 )
 P106=( n OPT_30D  1 )
 #== Parameter 2 ==================================
+P201=( n PDF_RNG_LN10  -4.  3.  71 )
+P202=( n LAT_RNG  -10.0  10.0 )
 #== Parameter 9 - I/O ============================
 P901=( n DAY1  1 )                 # the earlist date-of-file among input files
 P902=( n NDAY_I  3 )               # number of days in one input file
-P903=( n MISSV  1.e32 )            # if no missing points, set 1.0
-P990=( s FID  pc )
+P903=( n MISSV  0. )
+P990=( s FID  pc  pc  pa  pa )
 P991=( s FILE_I_HEAD  "/hippo0/HG2AMIP/$expname" )
 P992=( s FILE_I_FORM  XXXX/${P101[2]}a.XXXX_XXXXXXXXXXXX00.nc  -999 )
 P993=( s FILE_I_XXXX  FID              FID  YYYY MM  DD        -999 )
-P994=( s VAR_I_NAME  dch_max )
+P994=( s VAR_I_NAME  dch_max  vars_cgw  u  v )
 #P990=( s FID  pb )
 #P992=( s FILE_I_FORM  XXXX/${P101[2]}a.XXXX_XXXXXXXXX.nc  -999 )
 #P993=( s FILE_I_XXXX  FID              FID  YYYY MM       -999 )
 #P994=( s VAR_I_NAME  precip_dc )
+P996=( s FILE_ALT  "/hippo0/HG2AMIP/$expname/invariant/L60_z.nc" )
+P997=( s VAR_ALT  ht  ht_1 )  # z_theta, z_rho
 P999=( s FILE_O  '' )
 #=================================================
 P102=( n YYYY  '' )
@@ -51,16 +55,16 @@ M=$M1        ;  while [ $M    -le $M2    ] ; do
   MM=$(( M ))  ;  if [ $M -lt 10 ] ; then MM="0$MM" ; fi
   P102[2]=$YYYY
   P103[2]=$MM
-  ODIR=$DATD/$expname/precip_pdf/$YYYY
-  P999[2]="$ODIR/${P101[2]}.precip_pdf.$YYYY.${MM}.nc"
+  ODIR=$DATD/$expname/dchm_pdf/$YYYY
+  P999[2]="$ODIR/${P101[2]}.dchm_pdf.$YYYY.${MM}.nc"
   if [ ! -d $ODIR ] ; then mkdir -p $ODIR ; fi
-#  [ -e ${P999[2]} ] && mv ${P999[2]} $ODIR/old.${P101[2]}.precip_pdf.$YYYY.${MM}.nc
+#  [ -e ${P999[2]} ] && mv ${P999[2]} $ODIR/old.${P101[2]}.dchm_pdf.$YYYY.${MM}.nc
 
   # create namelist --------------------
   cr_file $F_NAMELIST
   cr_nl $F_NAMELIST ANALCASE "${P101[*]}" "${P102[*]}" "${P103[*]}" "${P104[*]}" "${P105[*]}" "${P106[*]}"
-#  cr_nl $F_NAMELIST PARAM "${P201[*]}"
-  cr_nl $F_NAMELIST FILEIO "${P901[*]}" "${P902[*]}" "${P903[*]}" "${P990[*]}" "${P991[*]}" "${P992[*]}" "${P993[*]}" "${P994[*]}" "${P999[*]}"
+  cr_nl $F_NAMELIST PARAM "${P201[*]}" "${P202[*]}"
+  cr_nl $F_NAMELIST FILEIO "${P901[*]}" "${P902[*]}" "${P903[*]}" "${P990[*]}" "${P991[*]}" "${P992[*]}" "${P993[*]}" "${P994[*]}" "${P996[*]}" "${P997[*]}" "${P999[*]}"
   cat $F_NAMELIST
 
   # compile and run --------------------
